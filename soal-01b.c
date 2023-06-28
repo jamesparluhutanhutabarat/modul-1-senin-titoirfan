393f3ea227583f33626527c7fabf68e8fef92f72
jamesparluhutanhutabarat <13219070@std.stei.itb.ac.id>
2023-06-28 11:48:22 +0700

    last

--- soal-01b.c
+++ soal-01b.c
@@ -1,8 +1,13 @@
+// Melakukan perhitungan dasar nilai murid
+
 #include <stdio.h>
 
-int main(){
-    printf("Hello World!\n");
-    printf("Hello World!\n");
+int main() {
+    int num_of_student;
 
+    printf("Masukkan jumlah murid: ");
+    scanf("%d", &num_of_student);
+
+    printf("Angka yang dimasukkan: %d\n", num_of_student);
     return 0;
 }
